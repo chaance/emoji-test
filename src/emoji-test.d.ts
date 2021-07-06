468d8a1d10e9616f3c5faea446128686a02f0def
Chance Strickland <chance.strickland@gmail.com>
2021-07-05 23:39:21 -0800

    config updates

---
+++ src/emoji-test.d.ts
@@ -0,0 +1,3 @@
+type EmojiList = Record<string, string>;
+let list: EmojiList;
+export default list;
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,40 +1,23 @@
-declare module 'regexgen' {
-  interface Regexgen {
-    (inputs: string[], flags?: string): RegExp;
-    Trie: typeof Trie;
-  }
+declare module "regexgen" {
+	interface Regexgen {
+		(inputs: string[], flags?: string): RegExp;
+		Trie: typeof Trie;
+	}
 
-  class State {
-    accepting: boolean;
-    transitions: Map<any, any>;
-    visit(visited?: Set<any>): void;
-  }
+	class State {
+		accepting: boolean;
+		transitions: Map<any, any>;
+		visit(visited?: Set<any>): void;
+	}
 
-  class Trie {
-    add(string: string): void;
-    addAll(strings: string[]): void;
-    minimize(): State;
-    toString(flags?: string): string;
-    toRegExp(flags?: string): RegExp;
-  }
+	class Trie {
+		add(string: string): void;
+		addAll(strings: string[]): void;
+		minimize(): State;
+		toString(flags?: string): string;
+		toRegExp(flags?: string): RegExp;
+	}
 
-  const regexgen: Regexgen;
-  export default regexgen;
+	const regexgen: Regexgen;
+	export default regexgen;
 }
-/*
-declare module 'regexgen' {
-  namespace regexgen {
-    class State {}
-    class Trie {
-      add(string: string): void;
-      addAll(strings: string[]): void;
-      minimize(): State;
-      toString(flags: string): string;
-      toRegExp(flags: string): RegExp;
-    }
-  }
-
-  function regexgen(inputs: string[], flags: string): RegExp;
-  export default regexgen;
-}
- */
