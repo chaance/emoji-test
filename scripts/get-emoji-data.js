05893ce55f5201faca8521569adf489a9dfbd409
Chance Strickland <chance.strickland@gmail.com>
2021-07-05 23:42:13 -0800

    emoji-test 13.1

---
+++ scripts/get-emoji-data.js
@@ -0,0 +1,57 @@
+const fs = require("fs");
+const path = require("path");
+
+const emojiTypeKeyMap = {
+	component: "_et_c",
+	"fully-qualified": "_et_f",
+	"minimally-qualified": "_et_m",
+	unqualified: "_et_u",
+};
+
+try {
+	let text = fs.readFileSync(path.join(__dirname, "emoji-test.txt"), {
+		encoding: "utf8",
+	});
+
+	for (let key of Object.keys(emojiTypeKeyMap)) {
+		text = text.replace(
+			new RegExp(`; ${key}`, "g"),
+			`; ${emojiTypeKeyMap[key]}`
+		);
+	}
+
+	fs.writeFileSync(
+		path.join(__dirname, "../src", "emoji-test.js"),
+		[
+			"// THIS IS A GENERATED FILE! Do not edit directly. See scripts/get-emoji-data.js",
+			`let list = ${JSON.stringify(getEmojiList(text))};`,
+			`export default list;`,
+		].join("\n")
+	);
+	console.log("Success!!");
+} catch (err) {
+	// TODO:
+	console.log("Error!!");
+	console.error(err);
+}
+
+function getEmojiList(src) {
+	let emojiList = {};
+	let lines = src.split("\n");
+
+	for (let line of lines) {
+		if (line && line[0] !== "#") {
+			let hashOffset = line.indexOf("#");
+			let data = line.substring(0, hashOffset);
+			let fields = data.split(";");
+			let codePoints = fields[0].trim().split(" ");
+			let status = fields[1].trim();
+			let emojiString = "";
+			for (let codePoint of codePoints) {
+				emojiString += String.fromCodePoint(parseInt(codePoint, 16));
+			}
+			emojiList[emojiString] = status;
+		}
+	}
+	return emojiList;
+}
