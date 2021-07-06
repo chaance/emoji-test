da9ce9c74e9ee4ceefe2581892b7837b0b0bb555
Chance Strickland <chance.strickland@gmail.com>
2021-07-05 23:43:00 -0800

    Fixed bug revealed by updated data file

--- src/index.ts
+++ src/index.ts
@@ -1,61 +1,60 @@
-import emojiList from './emoji-test';
-import regexgen from 'regexgen';
+import emojiList from "./emoji-test";
+import regexgen from "regexgen";
 
 const emojiTypeKeyMap = {
-  component: '_et_c',
-  'fully-qualified': '_et_f',
-  'minimally-qualified': '_et_m',
-  unqualified: '_et_u',
+	component: "_et_c",
+	"fully-qualified": "_et_f",
+	"minimally-qualified": "_et_m",
+	unqualified: "_et_u",
 };
 
 function getEmojiTestPatterns() {
-  let result = {} as RegexResults;
-  let emojiKeys = Object.keys(emojiList).sort(
-    // Decreasing length (critical!)
-    (a, b) => [...b].length - [...a].length
-  );
+	let result = {} as RegexResults;
+	let emojiKeys = Object.keys(emojiList).sort(
+		(a, b) => [...b].length - [...a].length || a.localeCompare(b)
+	); // Decreasing length (critical!) then lexicographically
 
-  let trieAll = new regexgen.Trie();
-  for (let key of emojiKeys) {
-    trieAll.add(key);
-  }
-  result.all = '(?:' + trieAll.toString('u') + ')';
+	let trieAll = new regexgen.Trie();
+	for (let key of emojiKeys) {
+		trieAll.add(key);
+	}
+	result.all = "(?:" + trieAll.toString("u") + ")";
 
-  let trieComponent = new regexgen.Trie();
-  for (let key of emojiKeys) {
-    if (emojiList[key] === emojiTypeKeyMap['component']) {
-      trieComponent.add(key);
-    }
-  }
-  result.component = '(?:' + trieComponent.toString('u') + ')';
+	let trieComponent = new regexgen.Trie();
+	for (let key of emojiKeys) {
+		if (emojiList[key] === emojiTypeKeyMap["component"]) {
+			trieComponent.add(key);
+		}
+	}
+	result.component = "(?:" + trieComponent.toString("u") + ")";
 
-  let trieKeyboard = new regexgen.Trie();
-  for (let key of emojiKeys) {
-    if (emojiList[key] === emojiTypeKeyMap['fully-qualified']) {
-      trieKeyboard.add(key);
-    }
-  }
-  result.keyboard = '(?:' + trieKeyboard.toString('u') + ')';
+	let trieKeyboard = new regexgen.Trie();
+	for (let key of emojiKeys) {
+		if (emojiList[key] === emojiTypeKeyMap["fully-qualified"]) {
+			trieKeyboard.add(key);
+		}
+	}
+	result.keyboard = "(?:" + trieKeyboard.toString("u") + ")";
 
-  let trieDisplay = new regexgen.Trie();
-  for (let key of emojiKeys) {
-    if (
-      emojiList[key] === emojiTypeKeyMap['minimally-qualified'] ||
-      emojiList[key] === emojiTypeKeyMap['unqualified']
-    ) {
-      trieDisplay.add(key);
-    }
-  }
-  result.display = '(?:' + trieDisplay.toString('u') + ')';
+	let trieDisplay = new regexgen.Trie();
+	for (let key of emojiKeys) {
+		if (
+			emojiList[key] === emojiTypeKeyMap["minimally-qualified"] ||
+			emojiList[key] === emojiTypeKeyMap["unqualified"]
+		) {
+			trieDisplay.add(key);
+		}
+	}
+	result.display = "(?:" + trieDisplay.toString("u") + ")";
 
-  return result;
+	return result;
 }
 
 export type RegexResults = {
-  all: string;
-  display: string;
-  component: string;
-  keyboard: string;
+	all: string;
+	display: string;
+	component: string;
+	keyboard: string;
 };
 
 export default getEmojiTestPatterns();
